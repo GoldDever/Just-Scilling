71ce83dbaa42616417dacf57723bc0be0eed16c8
GoldDever <69146353+GoldDever@users.noreply.github.com>
2021-01-21 20:19:10 +0600

    Rolling Back

---
+++ Model/src/main/java/com.altyn/Application.java
@@ -0,0 +1,11 @@
+package com.altyn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringAppication.run(Application.class, args);
+    }
+}
