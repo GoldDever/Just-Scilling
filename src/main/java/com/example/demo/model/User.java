ede3b06c3cd77599647b1f8e99f340fb101f1cbb
GoldDever <69146353+GoldDever@users.noreply.github.com>
2021-01-21 20:50:41 +0600

    User Created

---
+++ src/main/java/com/example/demo/model/User.java
@@ -0,0 +1,27 @@
+package com.example.demo.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "age")
+    private int age;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "password")
+    private String password;
+}
